830676468448f2e139e00d0c2aa05251dce90b29
doggles <8885886+doggles@users.noreply.github.com>
2017-08-10 22:14:28 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 set -u
+PROJECT=logindaemon
+IDENTIFIER=com.doggles.$PROJECT
 ROOT_DIR=$(dirname "$0")
 
 # check if run.sh and plist are in folder
-if [ ! -f "$ROOT_DIR"/com.doggles.logindaemon.plist ] || [ ! -f "$ROOT_DIR"/run.sh ]; then
+if [ ! -f "$ROOT_DIR"/$IDENTIFIER.plist ] || [ ! -f "$ROOT_DIR"/run.sh ]; then
 	echo "Required files not found"
 	exit 11
 fi
@@ -14,24 +16,24 @@ fi
 # create pkgbuild staging dirs
 mkdir -p \
 	"$ROOT_DIR"/.build/ROOT/Library/LaunchDaemons \
-	"$ROOT_DIR"/.build/ROOT/usr/local/logindaemon \
+	"$ROOT_DIR"/.build/ROOT/usr/local/$PROJECT \
 	"$ROOT_DIR"/.build/scripts
 
 # copy plist
 cp \
-	"$ROOT_DIR"/com.doggles.logindaemon.plist \
+	"$ROOT_DIR"/$IDENTIFIER.plist \
 	"$ROOT_DIR"/.build/ROOT/Library/LaunchDaemons
 
 # copy script
 cp \
 	"$ROOT_DIR"/run.sh \
-	"$ROOT_DIR"/.build/ROOT/usr/local/logindaemon
+	"$ROOT_DIR"/.build/ROOT/usr/local/$PROJECT
 
 # create postinstall
 cat > "$ROOT_DIR"/.build/scripts/postinstall <<SCRIPT
 #!/usr/bin/env bash
-chmod +x /usr/local/logindaemon/run.sh
-launchctl load -w /Library/LaunchDaemons/com.doggles.logindaemon.plist
+chmod +x /usr/local/$PROJECT/run.sh
+launchctl load -w /Library/LaunchDaemons/$IDENTIFIER.plist
 SCRIPT
 
 # build package
@@ -39,8 +41,8 @@ pkgbuild \
 	--root "$ROOT_DIR"/.build/ROOT \
 	--scripts "$ROOT_DIR"/.build/scripts \
 	--ownership recommended \
-	--identifier com.doggles.logindaemon \
-	"$ROOT_DIR"/deploy.pkg
+	--identifier $IDENTIFIER \
+	"$ROOT_DIR"/$PROJECT.pkg
 
 # cleanup
 _cleanup() { rm -rf "$ROOT_DIR"/.build; }
